ec32c188644b0caad2f5f91c49759a13a2190e3e
samisymbian <samisymbian@gmail.com>
2023-10-03 09:58:54 -0700

    hot fix

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,7 +1,12 @@
 #include "main.h"
 
-int main(void)
-{
-    _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
-    return (0);
+/**
+ * main - prints a quote
+ * @c: the character to be checked
+ * Return: 0 always
+ */
+int main(int c)
+{_puts("\"At the end of the day, my goal was to
+		    be the best hacker\"\n\t- Kevin Mitnick");
+return (0);
 }
